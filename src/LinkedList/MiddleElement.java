77c15bb256b86cc4ec34df461cd63d376846fd60
Shubham Manjhi <Shubham.Manjhi@blueyonder.com>
2024-06-25 00:04:15 +0530

    Delete Middle Element LinkedList

--- src/LinkedList/MiddleElement.java
+++ src/LinkedList/MiddleElement.java
@@ -14,6 +14,22 @@ public class MiddleElement {
         }
     }
 
+    public ListNode deleteMiddle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow_ptr = head;
+        ListNode fast_ptr = head.next.next;
+        while (fast_ptr != null && fast_ptr.next != null) {
+            fast_ptr = fast_ptr.next.next;
+            slow_ptr = slow_ptr.next;
+        }
+        slow_ptr.next = slow_ptr.next.next;
+        return head;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList llist = new SinglyLinkedList();
         for (int i=5; i>0; --i) {
